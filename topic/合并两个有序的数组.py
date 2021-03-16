de91d6820dac8f82b9a8c1b29f87db84e4d02521
LjFrr <52402130+LjFrr@users.noreply.github.com>
2021-03-16 11:26:30 +0800

    Update 合并两个有序的数组.py

--- topic/合并两个有序的数组.py
+++ topic/合并两个有序的数组.py
@@ -1,1 +1,12 @@
-
+#
+# 
+# @param A int整型一维数组 
+# @param B int整型一维数组 
+# @return void
+#
+class Solution:
+    def merge(self , A, m, B, n):
+        # write code here
+        A[:] = A[:m] + B[:n]
+        A.sort()
+        return A
