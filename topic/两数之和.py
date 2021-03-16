a4ffb6e137c361f5e8b6ac8a35d4c83f19fa1b95
LjFrr <52402130+LjFrr@users.noreply.github.com>
2021-03-16 10:28:55 +0800

    Update 两数之和.py

--- topic/两数之和.py
+++ topic/两数之和.py
@@ -1,1 +1,23 @@
-
+#
+# 
+# @param numbers int整型一维数组 
+# @param target int整型 
+# @return int整型一维数组
+#
+class Solution:
+    def twoSum(self , numbers , target ):
+        # write code here
+        res = [0,0]#保存结果
+        mp = {}#定义一个哈希表存储numbers[i]和对应的下标
+        for i in range(len(numbers)):#进行标记
+            mp[numbers[i]] = i
+        for i in range(len(numbers)):
+             #每遍历一个numbers[i]就去对应的mp里找有没有target - numbers[i]
+             #如果有就返回结果
+             #如果没有就找下一个
+            if target - numbers[i] in mp:
+                if mp[target - numbers[i]] != i:
+                    res[0] = i + 1
+                    res[1] = mp[target - numbers[i]] + 1
+                    return res
+        return res
