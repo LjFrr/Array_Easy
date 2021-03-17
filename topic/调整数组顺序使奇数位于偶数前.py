ce579c833d602d7ea630e15a724307241deb6a33
LjFrr <52402130+LjFrr@users.noreply.github.com>
2021-03-17 17:29:53 +0800

    Update 调整数组顺序使奇数位于偶数前.py

--- topic/调整数组顺序使奇数位于偶数前.py
+++ topic/调整数组顺序使奇数位于偶数前.py
@@ -1,1 +1,18 @@
-
+#
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+#
+# 
+# @param array int整型一维数组 
+# @return int整型一维数组
+#
+class Solution:
+    def reOrderArray(self , array ):
+        # write code here
+        odd = []
+        even = []
+        for i in array:
+            if i%2 == 1:
+                odd.append(i)
+            else:
+                even.append(i)
+        return odd + even
