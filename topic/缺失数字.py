0db7deda4211986d5803aba710f28c8a96956774
LjFrr <52402130+LjFrr@users.noreply.github.com>
2021-03-17 17:29:27 +0800

    Update 缺失数字.py

--- topic/缺失数字.py
+++ topic/缺失数字.py
@@ -1,1 +1,11 @@
-
+#
+# 找缺失数字
+# @param a int整型一维数组 给定的数字串
+# @return int整型
+#
+class Solution:
+    def solve(self , a ):
+        # write code here
+        preSum = (len(a) * (len(a) + 1)) / 2
+        curSum = sum(a)
+        return preSum - curSum
