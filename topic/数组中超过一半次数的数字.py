0eaeddebba4f978507d6c538c162f8c1a1fb06f3
LjFrr <52402130+LjFrr@users.noreply.github.com>
2021-03-17 17:29:01 +0800

    Update 数组中超过一半次数的数字.py

--- topic/数组中超过一半次数的数字.py
+++ topic/数组中超过一半次数的数字.py
@@ -1,1 +1,24 @@
-
+# -*- coding:utf-8 -*-
+class Solution:
+    def MoreThanHalfNum_Solution(self, numbers):
+        # write code here
+        length = len(numbers)
+        if (length == 0):
+            return 0
+        elif(length == 1):
+            return numbers[0]
+        else:
+            # 初始化
+            num = numbers[0]
+            count = 1
+            for i in range(1,length):
+                if numbers[i] == num:
+                    count += 1
+                else:
+                    count -= 1
+                if count == 0:
+                    num = numbers[i]
+                    count = 1
+            if(numbers.count(num) > len(numbers)/2):
+                return num
+            return 0
