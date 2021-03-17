fd60e96a337d193faa6417b22fc48ecc14ad44c6
LjFrr <52402130+LjFrr@users.noreply.github.com>
2021-03-17 17:28:31 +0800

    Update 数字在升序数组中出现的次数.py

--- topic/数字在升序数组中出现的次数.py
+++ topic/数字在升序数组中出现的次数.py
@@ -1,1 +1,9 @@
-
+# -*- coding:utf-8 -*-
+class Solution:
+    def GetNumberOfK(self, data, k):
+        # write code here
+        count = 0
+        for i in data:
+            if k == i:
+                count += 1
+        return count
