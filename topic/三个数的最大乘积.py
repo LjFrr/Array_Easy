260899f2034372cf2669c60a507a2fa912a25a04
LjFrr <52402130+LjFrr@users.noreply.github.com>
2021-03-17 17:30:25 +0800

    Update 三个数的最大乘积.py

--- topic/三个数的最大乘积.py
+++ topic/三个数的最大乘积.py
@@ -1,1 +1,10 @@
-
+#
+# 最大乘积
+# @param A int整型一维数组 
+# @return long长整型
+#
+class Solution:
+    def solve(self , A ):
+        # write code here
+        A.sort()
+        return max(A[0]*A[1]*A[-1], A[-1]*A[-2]*A[-3])
