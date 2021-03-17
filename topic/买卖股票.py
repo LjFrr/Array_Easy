235e46a8395509effc5da2f8fd54294eadbf849f
LjFrr <52402130+LjFrr@users.noreply.github.com>
2021-03-17 17:27:47 +0800

    Update 买卖股票.py

--- topic/买卖股票.py
+++ topic/买卖股票.py
@@ -1,1 +1,16 @@
-
+#
+# 
+# @param prices int整型一维数组 
+# @return int整型
+#
+class Solution:
+    def maxProfit(self , prices ):
+        # write code here
+        if len(prices) == 0:
+            return 0
+        minPrice = prices[0]
+        profit = 0
+        for i in range(len(prices)):
+            minPrice = min(minPrice, prices[i])
+            profit = max(profit, prices[i]-minPrice)
+        return profit
